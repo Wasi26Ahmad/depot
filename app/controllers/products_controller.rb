81daf700a4db19c81d07c2509a2d6aa4bc52d15f
Wasi Ahmad <wasiahmad0569@gmail.com>
2026-05-03 17:22:28 +0600

    Action Cable

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,8 +38,9 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.', status: :see_other }
+        format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+        @product.broadcast_replace_later_to 'products', partial: 'store/product'
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
